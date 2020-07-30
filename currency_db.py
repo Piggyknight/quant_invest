683d7f19edca4b3edcdd1b79f187d1e7fa5d9903
Piggyknight <pigknight@hotmail.com>
2020-07-30 09:08:10 +0800

    implement market

--- currency_db.py
+++ currency_db.py
@@ -1,55 +1,12 @@
 # -*- coding:utf-8 -*-
 
+from typing import List
 from datetime import datetime
 import csv
 import os
 
 _print_format = 'year:%d, month:%d, day:%d, time:%d, open:%.4f, high:%.4f, low:%.4f, close:%.4f'
 _data_format = '%Y.%m.%d %H:%M'
-
-
-class CurrencyDb:
-    """
-     contain info load from the split meta trade 5 csv file
-     PS: one important assumption: meta trade's csv is always sorted by time
-    """
-
-    def __init__(self):
-        self.db = []
-
-    def Load(self, db_file: str) -> None:
-        # 1. check if file is exist
-        is_file_exist = os.path.exists(db_file)
-        if not is_file_exist:
-            print("[currency_db]: File not exist: %s" % db_file)
-
-        # 2. read csv file into CurrencyRow
-        with open(db_file, 'r', encoding='utf-8-sig') as csv_file:
-            reader = csv.reader(csv_file)
-
-            print("[currency_db]Start analyze csv file...")
-            begin_size = len(self.db)
-            for row in reader:
-                if 0 == len(row):
-                    continue;
-
-                cr = CurrencyRow()
-
-                # 2.1 convert string to datetime struct
-                d_str = row[0]
-                cr.time = datetime.strptime(d_str, _data_format)
-
-                # 2.2 convert other string into float 
-                cr.open = float(row[1])
-                cr.high = float(row[2])
-                cr.low = float(row[3])
-                cr.close = float(row[4])
-
-                # 2.3 store into db
-                self.db.append(cr)
-
-            end_size = len(self.db)
-            print("[currency_db]Finished loading file %s, %d rows added" % (db_file, end_size - begin_size))
 
 
 class CurrencyRow:
@@ -71,3 +28,56 @@ class CurrencyRow:
                                 self.close)
 
     __repr__ = __str__
+
+
+class CurrencyDb:
+    """
+     contain info load from the split meta trade 5 csv file
+     PS: one important assumption: meta trade's csv is always sorted by time
+    """
+
+    def __init__(self):
+        self.db = List[CurrencyRow]
+
+    def Load(self, db_file: str) -> int:
+        # 1. check if file is exist
+        is_file_exist = os.path.exists(db_file)
+        if not is_file_exist:
+            print("[market]: File not exist: %s" % db_file)
+            return -1
+
+        # 2. read csv file into CurrencyRow
+        with open(db_file, 'r', encoding='utf-8-sig') as csv_file:
+            reader = csv.reader(csv_file)
+
+            print("[market]Start analyze csv file...")
+            begin_size = len(self.db)
+            for row in reader:
+                if 0 == len(row):
+                    continue
+
+                cr = CurrencyRow()
+
+                # 2.1 convert string to datetime struct
+                d_str = row[0]
+                cr.time = datetime.strptime(d_str, _data_format)
+
+                # 2.2 convert other string into float 
+                cr.open = float(row[1])
+                cr.high = float(row[2])
+                cr.low = float(row[3])
+                cr.close = float(row[4])
+
+                # 2.3 store into db
+                self.db.append(cr)
+
+            end_size = len(self.db)
+            print("[market]Finished loading file %s, %d rows added" % (db_file, end_size - begin_size))
+
+        return 0
+
+    def Get(self, idx: int) -> CurrencyRow:
+        if idx > len(self.db) or idx < 0:
+            print("[market]idx(%d) out of range when getting data from CurrencyDb", idx)
+        return self.db[idx]
+
--- currency_market.py
+++ currency_market.py
@@ -1,50 +1,42 @@
 # -*- coding:utf-8 -*-
 
+from typing import List
 from currency_db import *
 
 
-
-# hanlde the currency db, and basic order operation
 class CurrencyMarket:
+    C_FILE_PATH_FORMAT = "./data/%d.csv"
+
+    # handle the currency db, and basic order operation
     def __init__(self):
-        self._orders={}
-        self._db = CureencyDb()
-        self._cur_idx = 0
-        self._id_gen = IdGenerator()
-        self._cmd_list = []
+        self.orders = {}
+        self.db = CurrencyDb()
+        self.cur_idx = 0
 
-    # input
-    # - years: list of yeas to simulate
-    # - no return
-    def Init(self, years):
-        # 1. loop the list of int yeras
+    def Init(self, years: List[int]):
+        """
+        :param years: list of yeas to simulate 
+        :return: error during init 
+        """
+        # 1. loop the list of int years
+        for year in years:
+            # 2. use year to create file path
+            file_path = self.C_FILE_PATH_FORMAT.format(year)
 
-        # 2. use year to create filepath
-
-        # 3. load file into db
-
-        # 4. create cmd list
-
-        # 5. reset order_id
+            # 3. load file into db
+            is_success = self.db.Load(file_path)
+            print("[market]Load db: %s, result: %d", file_path, is_success)
 
         return
 
-    def NextData(self):
+    def NextData(self) -> CurrencyRow:
         # 1. safe check
-        if None == self._db:
+        if self.db is None:
+            print("[market]db is null")
             return None
 
         # 2. return next CurrencyRow, update the counter
+        row = self.db.Get(self.cur_idx)
+        self.cur_idx = self.cur_idx + 1
 
-    # process given list of cmds, return the list of orders
-    def ExecuteCmd(self, cmds):
-
-        for cmd in cmds:
-            _cmd_list[cmd._type].Do(cmd.data, self._orders)
-
-        return False
-
-
-
-
-
+        return row
